ae1e3ae265932c5ed26fd4f1dbc1ccbc03909071
Sleon4 <sergioleon4004@hotmail.com>
2024-02-25 18:06:29 -0500

    docs: comment block added to document

--- src/DependencyInjection/Container.php
+++ src/DependencyInjection/Container.php
@@ -10,20 +10,46 @@ use DI\ContainerBuilder;
 use Lion\Helpers\Str;
 use ReflectionClass;
 use ReflectionFunction;
-use ReflectionMethod;
+use ReflectionFunctionAbstract;
 use ReflectionParameter;
 
+/**
+ * Container to generate dependency injection
+ *
+ * @package Lion\DependencyInjection
+ */
 class Container
 {
+    /**
+     * [Object of class DIContainer]
+     *
+     * @var DIContainer $container
+     */
     private DIContainer $container;
+
+    /**
+     * [Object of class Str]
+     *
+     * @var Str $str
+     */
     private Str $str;
 
+    /**
+     * Class constructor
+     */
     public function __construct()
     {
         $this->container = (new ContainerBuilder())->useAutowiring(true)->useAttributes(true)->build();
         $this->str = new Str();
     }
 
+    /**
+     * Get files from a defined path
+     *
+     * @param  string $folder [Defined route]
+     *
+     * @return array<string>
+     */
     public function getFiles(string $folder): array
     {
         $files = [];
@@ -44,6 +70,15 @@ class Container
         return $files;
     }
 
+    /**
+     * Gets the namespace of a class through a defined path
+     *
+     * @param  string $file [File path]
+     * @param  string $namespace [Namespace for the file]
+     * @param  string $split [Separator to obtain the namespace]
+     *
+     * @return string
+     */
     public function getNamespace(string $file, string $namespace, string $split = '/'): string
     {
         $splitFile = explode($split, $file);
@@ -51,7 +86,15 @@ class Container
         return $this->str->of("{$namespace}{$splitFile[1]}")->replace("/", "\\")->replace('.php', '')->trim()->get();
     }
 
-    private function getParameters(ReflectionMethod|ReflectionFunction $method, array $params = []): array
+    /**
+     * Gets the parameters of a function
+     *
+     * @param  ReflectionFunctionAbstract $method [Method obtained]
+     * @param  array $params [Array of defined parameters]
+     *
+     * @return array
+     */
+    private function getParameters(ReflectionFunctionAbstract $method, array $params = []): array
     {
         $args = [];
 
@@ -70,6 +113,15 @@ class Container
         return $args;
     }
 
+    /**
+     * Inject dependencies into a method of a class
+     *
+     * @param  object $object [Class object]
+     * @param  string $method [Method name]
+     * @param  array $params [Array of defined parameters]
+     *
+     * @return mixed
+     */
     public function injectDependenciesMethod(object $object, string $method, array $params = []): mixed
     {
         $method = (new ReflectionClass($object))->getMethod($method);
@@ -77,6 +129,14 @@ class Container
         return $method->invoke($object, ...$this->getParameters($method, $params));
     }
 
+    /**
+     * Inject dependencies to a callback
+     *
+     * @param  Closure $closure [Defined callback]
+     * @param  array $params [Array of defined parameters]
+     *
+     * @return mixed
+     */
     public function injectDependenciesCallback(Closure $closure, array $params = []): mixed
     {
         $method = new ReflectionFunction($closure);
@@ -84,6 +144,15 @@ class Container
         return $method->invoke(...$this->getParameters($method, $params));
     }
 
+    /**
+     * Inject dependencies to methods of a class that have the annotation
+     * '@required'
+     *
+     * @param  object $object [Class object]
+     * @param  array $params [Array of defined parameters]
+     *
+     * @return object
+     */
     public function injectDependencies(object $object, array $params = []): object
     {
         foreach ((new ReflectionClass($object))->getMethods() as $method) {
@@ -99,6 +168,14 @@ class Container
         return $object;
     }
 
+    /**
+     * Gets the data type of the parameters obtained
+     *
+     * @param  ReflectionParameter $parameter [Defined parameter of type
+     * ReflectionParameter]
+     *
+     * @return null|string
+     */
     private function getParameterClassName(ReflectionParameter $parameter): ?string
     {
         $type = $parameter->getType();
